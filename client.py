c817e0b269a90c88fd959d8dda504dfac41df753
RadeckiR <radecki.radoslaw@gmail.com>
2021-01-25 23:33:58 +0100

    update on client

--- client.py
+++ client.py
@@ -16,7 +16,7 @@ def createStream(Music):
     return stream
 
 
-def connect2(s,port):
+def connect2(s, port):
     # host = input()
     # port = int(input())
     # host = "127.0.0.1"
@@ -30,85 +30,139 @@ def connect2(s,port):
     # s.send('play'.encode())
     print("connected")
 
+
 def run(s, stream):
-    data = s.recv(1024)
-    #print(data)
+    data = s.recv(16)
+    # print(data)
+    playing = data
+
     while data != '':
         stream.write(data)
 
-        data = s.recv(1024)
+        data = s.recv(16)
     stream.close()
 
 
-def sendFile(sock,songname):
+def sendFile(sock, songname):
     filename = songname  # In the same folder or path is this file running must the file you want to tranfser to be
     f = open(filename, 'rb')
-    l = f.read(4096)
+    l = f.read(512)
     # print(l)
     # size=(sys.getsizeof(l))
     # print(size)
     # print(size.to_bytes(8, byteorder='big'))
     while (l):
         sock.send(l)
-        #print('Sent ', repr(l))
-        l = f.read(4096)
+        # print('Sent ', repr(l))
+        l = f.read(512)
     f.close()
 
-def sendMessage(sock,message):
+
+def sendMessage(sock, message):
     sock.send(message)
-
-
-
 
 
 def main():
     silence = True
     communicationSocket = socket.socket()  # creates socket
     connect2(communicationSocket, 27)  # connects socket via hostname and port from input arguments
+    init = True
     while True:
-        # test()
-        #message=communicationSocket.recv(1024)
-        #print(message)
+
+        if init == True:
+            message = communicationSocket.recv(1024)
+            message = str(message)
+            message = message.replace(r'\n', '\n')
+            print(message)
+            init = False
+
+        print('test')
         a = input()
+
         if a == 'send':
             print('you want to upload new song.')
-            sendMessage(communicationSocket,'send'.encode())
+            sendMessage(communicationSocket, 'send'.encode())
             message = communicationSocket.recv(1024)
             print(message)
-            print("Write a name of your song(only .wav format, make sure its in your folder and to include .wav at the end")
-            songname=input()
-            sendMessage(communicationSocket,songname.encode())
-            sendFile(communicationSocket,songname)
+            print(
+                "Write a name of your song(only .wav format, make sure its in your folder and to include .wav at the end")
+            songname = input()
+            sendMessage(communicationSocket, songname.encode())
+            sendFile(communicationSocket, songname)
+            sendMessage(communicationSocket, "end".encode())
             print("song sent.")
         if a == 'play':
-
+            print('playing')
             if silence:
                 playSocket = socket.socket()  # creates socket
                 connect2(playSocket, 14)
                 Music = pyaudio.PyAudio()  # initiate pyaudio variable
                 stream = createStream(Music)
                 start_new_thread(run, (playSocket, stream))
+                print("out of thread")
                 silence = False
             else:
-                #Music.terminate()
+                # Music.terminate()
                 playSocket.close()
 
-
+        if a == 'skip':
+            print('you want to skip some songs.')
+            sendMessage(communicationSocket, 'skip'.encode())
+            message = communicationSocket.recv(1024)
+            print(message)
+            if "Sorry".encode() not in message:
+                print("Current song order:")
+                message = communicationSocket.recv(1024)
+                message = message.replace(r'\n', '\n')
+                print(message)
+                print('how many songs you want to skip')
+                songSkip = input()
+                sendMessage(communicationSocket, songSkip.encode())
+                message = communicationSocket.recv(1024)
+                print(message)
+                while "Wrong input values".encode() in message:
+                    songSkip = input()
+                    sendMessage(communicationSocket, songSkip.encode())
+                    message = communicationSocket.recv(1024)
+                    print(message)
+                print('successfully skipped ', songSkip, 'songs')
         if a == 'quit':
             Music.terminate()
 
+        if a == 'list':
+            print('receiving list of songs:')
+            sendMessage(communicationSocket, 'list'.encode())
+            message = communicationSocket.recv(1024)
+            message = str(message)
+            message = message.replace(r'\n', '\n')
+            print(message)
+
+        if a == 'edit':
+            print('you want to edit playlist.')
+            sendMessage(communicationSocket, 'edit'.encode())
+            message = communicationSocket.recv(1024)
+            print(message)
+            if "Sorry".encode() not in message:
+                print("Current song order:")
+                message = communicationSocket.recv(1024)
+                message = message.replace(r'\n', '\n')
+                print(message)
+                print("Write your song order")
+                songOrder = input()
+                sendMessage(communicationSocket, songOrder.encode())
+                message = communicationSocket.recv(1024)
+                print(message)
+                print(len(message))
+                while "Wrong input values".encode() in message:
+                    print('trying again..')
+                    songOrderRetry = input()
+                    sendMessage(communicationSocket, songOrderRetry.encode())
+                    message = communicationSocket.recv(1024)
+                    print(message)
+                print("success")
+
         # if a == 'edit':
         #     #prosba o edycje
-
-
-
-
-
-
-
-
-
-
 
 
 def test():
